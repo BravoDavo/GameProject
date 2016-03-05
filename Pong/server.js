500cd71216c42e375aef6bfb57d1ab6992b2e47b
BravoDavo <david.s.jaesch@student.fh-kiel.de>
2016-03-05 21:21:49 +0100

    Multiplayer Sockets working

--- Pong/server.js
+++ Pong/server.js
@@ -1,29 +1,96 @@
-/**
- * Created by David on 05.03.2016.
- */
-var express     =   require('express');
-var bodyParser =    require('body-parser');
-
-var app         =   express();
-var path        =   require('path');
+var express = require('express');
+var app = express();
 var ejs         =   require('ejs');
-//var socketIO    =   require('./SocketIO');
-var routes  =   require('./routes/index.js');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var Player = require("./Player").Player;
 
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', routes);
 
-var server      =   app.listen(3000, function(){
-    console.log("Server started and listening on Port 3000");
+app.get('/', function(req, res){
+    res.render('index.html');
 });
-//socketIO.initWebSocket(server);
+
+var util = require("util"),
+    io = require("socket.io");
+var socket,
+    players;
+
+var setEventHandlers = function() {
+    socket.sockets.on("connection", onSocketConnection);
+};
+
+function onSocketConnection(client) {
+    util.log("New player has connected: "+client.id);
+    client.on("disconnect", onClientDisconnect);
+    client.on("new player", onNewPlayer);
+    client.on("move player", onMovePlayer);
+    client.on("remove player", onRemovePlayer);
+};
+
+function onClientDisconnect() {
+    util.log("Player has disconnected: "+this.id);
+};
+
+function onNewPlayer(data) {
+    var newPlayer = new Player(data.x, data.y);
+    newPlayer.id = this.id;
+    this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()});
+    var i, existingPlayer;
+    for (i = 0; i < players.length; i++) {
+        existingPlayer = players[i];
+        this.emit("new player", {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()});
+    };
+    players.push(newPlayer);
+};
+
+function onMovePlayer(data) {
+    var movePlayer = playerById(this.id);
+
+    if (!movePlayer) {
+        util.log("Player not found: "+this.id);
+        return;
+    };
+
+    movePlayer.setX(data.x);
+    movePlayer.setY(data.y);
+
+    this.broadcast.emit("move player", {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY()});
+};
+
+function onRemovePlayer(data) {
+    var removePlayer = playerById(this.id);
+
+    if (!removePlayer) {
+        util.log("Player not found: "+this.id);
+        return;
+    };
+
+    players.splice(players.indexOf(removePlayer), 1);
+    this.broadcast.emit("remove player", {id: this.id});
+};
+
+function init() {
+    players = [];
+    socket = io.listen(server.listen(3000));
+    console.log("Multiplayer app listening on port 3000");
+    setEventHandlers();
+};
+
+init();
+
+function playerById(id) {
+    var i;
+    for (i = 0; i < players.length; i++) {
+        if (players[i].id == id)
+            return players[i];
+    };
+
+    return false;
+};
 
 
 
